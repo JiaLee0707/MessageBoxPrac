1fe0772dca5803f1f672530e500a2a82854dc707
JiaLee0707 <s2018s37@e-mirim.hs.kr>
2020-06-16 10:41:16 +0900

    초기 커밋.

---
+++ MessageBoxPrac/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MessageBoxPrac
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("메시지박스.쇼");
+            MessageBox.Show("메시지박스.쇼", "제목");
+            MessageBox.Show("메시지박스.쇼", "제목", MessageBoxButtons.RetryCancel);
+            MessageBox.Show("메시지박스.쇼", "제목", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Asterisk);
+        }
+    }
+}
